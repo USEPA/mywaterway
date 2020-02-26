b2e7b91e6d610cc24b67eda181f29ce307ea829d
Julie McBride <julmcbride@gmail.com>
2020-02-26 13:01:33 -0500

    add test to confirm clicking the disclaimer button displays the disclaimer popup and disclaimer close button closes popup

---
+++ app/client/cypress/integration/home/disclaimer.modal.spec.js
@@ -0,0 +1,20 @@
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it(`Disclaimer button opens modal`, () => {
+    cy.findByText('Disclaimer').click();
+  });
+
+  it(`Disclaimer close button closes modal`, () => {
+    cy.get('body').then(($body) => {
+      if ($body.find('button[title="Close disclaimer"]').length > 0) {
+        cy.get('button[title="Close disclaimer"]').then(($header) => {
+          if ($header.is(':visible')) {
+            cy.findByText('Close disclaimer').click();
+          }
+        });
+      }
+    });
+  });
+});
