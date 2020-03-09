390513eab1e384e73eb48bba88252f0c3a8c7de4
Julie McBride <julmcbride@gmail.com>
2020-03-09 15:33:03 -0400

    add glossary button toggle test

---
+++ app/client/cypress/integration/home/glossary.toggle.spec.js
@@ -0,0 +1,20 @@
+describe('Homepage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it(`Glossary button toggles slide content`, () => {
+    //.findByText('Glossary')
+    cy.get('button[title="Glossary"]').click();
+  });
+  it(`Glossary close button closes slide content`, () => {
+    cy.get('body').then(($body) => {
+      if ($body.find('button[title="Close glossary"]').length > 0) {
+        cy.get('button[title="Close glossary"]').then(($header) => {
+          if ($header.is(':visible')) {
+            cy.findByText('Close glossary').click();
+          }
+        });
+      }
+    });
+  });
+});
