92a5793d0ea6e4aff326cb981de8089efef0878e
Caleb Schwind <caleb.schwind@erg.com>
2020-03-12 10:16:58 -0400

    Uncomment test case.

--- app/client/cypress/integration/state/water-quality-overview/subTab.spec.js
+++ app/client/cypress/integration/state/water-quality-overview/subTab.spec.js
@@ -71,28 +71,28 @@ describe('Water Quality Overview Sub Tabs', () => {
       .should('exist');
   });
 
-  // it('Navigating to a sub-tab selection that shows bar chart and pie chart (e.g. )', () => {
-  //   // verify the water quality overview content loaded prior to other tests
-  //   cy.findByText('Water Quality', { timeout: 20000 }).should('exist');
-  //   cy.get('button:contains(Aquatic Life)[role=tab]').click();
+  it('Navigating to a sub-tab selection that shows bar chart and pie chart (e.g. )', () => {
+    // verify the water quality overview content loaded prior to other tests
+    cy.findByText('Water Quality', { timeout: 20000 }).should('exist');
+    cy.get('button:contains(Aquatic Life)[role=tab]').click();
 
-  //   // wait for the surveys service to finish
-  //   cy.wait(15000);
+    // wait for the surveys service to finish
+    cy.wait(15000);
 
-  //   // select a dropdown item that has the pie chart
-  //   cy.get('.Select__control:contains(Coastal Waters)')
-  //     .filter(':visible')
-  //     .click();
-  //   cy.findByText('Rivers and Streams').click();
+    // select a dropdown item that has the pie chart
+    cy.get('.Select__control:contains(Coastal Waters)')
+      .filter(':visible')
+      .click();
+    cy.findByText('Rivers and Streams').click();
 
-  //   // verify the pie chart is not there and the bar chart is
-  //   cy.get('p')
-  //     .filter(':visible')
-  //     .contains('State statistical surveys provide an overall picture')
-  //     .should('exist');
-  //   cy.get('p')
-  //     .filter(':visible')
-  //     .contains('Targeted monitoring provides information')
-  //     .should('exist');
-  // });
+    // verify the pie chart is not there and the bar chart is
+    cy.get('p')
+      .filter(':visible')
+      .contains('State statistical surveys provide an overall picture')
+      .should('exist');
+    cy.get('p')
+      .filter(':visible')
+      .contains('Targeted monitoring provides information')
+      .should('exist');
+  });
 });
