ab0017d40110b032f62cca0e84cf5c6579050ff3
Caleb Schwind <caleb.schwind@erg.com>
2020-03-12 14:23:27 -0400

    Added a custom command for mocking the geolocation api. Added in a test for a user allowing geolocation and a user not allowing geolocation.

--- app/client/cypress/integration/community/search.spec.js
+++ app/client/cypress/integration/community/search.spec.js
@@ -9,4 +9,22 @@ describe('Community search', () => {
     cy.findByText('Go').click();
     cy.url().should('include', `/community/${zip}/overview`);
   });
+
+  it('Clicking the “Use My Location” button, with geolocation enabled, properly geolocates user and routes to the community overview page for the user’s location', () => {
+    cy.mockGeolocation();
+
+    cy.findByText('Use My Location').click();
+
+    cy.url().should('match', /community\/.*\/overview/);
+    cy.findByText('Your Waters: What We Know');
+  });
+
+  it('Clicking the “Use My Location” button, with geolocation disabled, displays an error and does not route', () => {
+    cy.mockGeolocation(true);
+
+    cy.findByText('Use My Location').click();
+
+    cy.url().should('equal', `${window.location.origin}/community`);
+    cy.findByText('Error Getting Location');
+  });
 });
