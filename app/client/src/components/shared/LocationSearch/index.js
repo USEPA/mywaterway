71d84ef783b8deb2527046cd32c82b511e72c221
Caleb Schwind <caleb.schwind@erg.com>
2020-05-06 10:15:24 -0400

    Tied the label to the input to fix an accessibility issue found by the lighthouse plugin.

--- app/client/src/components/shared/LocationSearch/index.js
+++ app/client/src/components/shared/LocationSearch/index.js
@@ -88,9 +88,11 @@ function LocationSearch({ route }: Props) {
         }
       }}
     >
-      <label className="sr-only">Location</label>
-
+      <label htmlFor="hmw-search-input" className="sr-only">
+        Location
+      </label>
       <Input
+        id="hmw-search-input"
         className="form-control"
         placeholder="Search by address, zip code, or place..."
         value={inputText}
