bf07e744649d094a40d211582e55b9ac8c6b9320
Caleb Schwind <caleb.schwind@erg.com>
2020-05-20 12:02:05 -0400

    Added polyfills for find, findIndex and entries.

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -2,6 +2,10 @@
 
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import 'core-js/features/array/find';
+import 'core-js/features/array/find-index';
+import 'core-js/features/object/entries';
+import findIndex from 'array.prototype.findindex';
 import smoothscroll from 'smoothscroll-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -17,6 +21,7 @@ import { GlossaryProvider } from 'contexts/Glossary';
 // errors
 import { defaultErrorBoundaryMessage } from 'config/errorMessages';
 
+findIndex.shim();
 smoothscroll.polyfill();
 
 // --- styled components ---
