d7734ff718d6ff1260265e26ca11c125e25ad03d
Caleb Schwind <caleb.schwind@erg.com>
2020-05-21 12:11:36 -0400

    Moved the 400, 500 and favicon files to the public folder, so it is included in the build.

--- app/server/app/server/routes/404.js
+++ app/server/app/server/routes/404.js
@@ -4,9 +4,7 @@ module.exports = function (app) {
   const router = express.Router();
 
   router.get('/', function (req, res, next) {
-    res
-      .status(404)
-      .sendFile(path.join(__dirname, 'public_other_pages', '400.html'));
+    res.status(404).sendFile(path.join(__dirname, 'public', '400.html'));
   });
 
   app.use('/404', router);
