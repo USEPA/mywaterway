5a240d641efb483337903ee5566e8935fda1cfda
oftheheadland <andrewvanness221@gmail.com>
2020-10-26 18:59:25 -0400

    move change location popup functions into their own useDynamicPopup hook to prevent duplicate code blocks in Upstream widget code

--- app/client/src/components/shared/MapWidgets/index.js
+++ app/client/src/components/shared/MapWidgets/index.js
@@ -10,11 +10,9 @@ import { LocationSearchContext } from 'contexts/locationSearch';
 import { FullscreenContext } from 'contexts/Fullscreen';
 import { useServicesContext } from 'contexts/LookupFiles';
 // utilities
-import {
-  shallowCompare,
-  getPopupContent,
-  getPopupTitle,
-} from 'components/pages/LocationMap/MapFunctions';
+import { shallowCompare } from 'components/pages/LocationMap/MapFunctions';
+// helpers
+import { useDynamicPopup } from 'utils/hooks';
 
 const basemapNames = [
   'Streets',
@@ -187,12 +185,12 @@ function MapWidgets({
     getUpstreamExtent,
     setUpstreamExtent,
     setErrorMessage,
-    getHucBoundaries,
-    getMapView,
-    resetData,
   } = React.useContext(LocationSearchContext);
 
   const services = useServicesContext();
+
+  const getDynamicPopup = useDynamicPopup();
+  const { getTitle, getTemplate } = getDynamicPopup();
 
   const {
     getFullscreenActive,
@@ -661,106 +659,6 @@ function MapWidgets({
     );
   }
 
-  var hucInfo = {
-    status: 'none',
-    data: null,
-  };
-
-  var lastLocation = null;
-  function getClickedHuc(location) {
-    return new Promise((resolve, reject) => {
-      const testLocation = {
-        latitude: location.latitude,
-        longitude: location.longitude,
-      };
-
-      // check if the location changed
-      if (
-        testLocation &&
-        lastLocation &&
-        testLocation.latitude === lastLocation.latitude &&
-        testLocation.longitude === lastLocation.longitude
-      ) {
-        // polls the dom, based on provided timeout, until the esri search input
-        // is added. Once the input is added this sets the id attribute and stops
-        // the polling.
-        function poll(timeout: number) {
-          if (['none', 'fetching'].includes(hucInfo.status)) {
-            setTimeout(poll, timeout);
-          } else {
-            resolve(hucInfo);
-          }
-        }
-
-        poll(1000);
-
-        return;
-      }
-
-      lastLocation = testLocation;
-      hucInfo = {
-        status: 'fetching',
-        data: null,
-      };
-
-      //get the huc boundaries of where the user clicked
-      const query = new Query({
-        returnGeometry: true,
-        geometry: location,
-        outFields: ['*'],
-      });
-
-      new QueryTask({ url: services.data.wbd })
-        .execute(query)
-        .then((boundaries) => {
-          if (boundaries.features.length === 0) return;
-
-          const { attributes } = boundaries.features[0];
-          hucInfo = {
-            status: 'success',
-            data: {
-              huc12: attributes.huc12,
-              watershed: attributes.name,
-            },
-          };
-          resolve(hucInfo);
-        })
-        .catch((err) => {
-          console.error(err);
-          reject(err);
-        });
-    });
-  }
-
-  // Wrapper function for getting the content of the popup
-  function getTemplate(graphic) {
-    // get the currently selected huc boundaries, if applicable
-    const hucBoundaries = getHucBoundaries();
-    const mapView = getMapView();
-    const location = mapView?.popup?.location;
-
-    // only look for huc boundaries if no graphics were clicked and the
-    // user clicked outside of the selected huc boundaries
-    if (
-      !location ||
-      (hucBoundaries &&
-        hucBoundaries.features.length > 0 &&
-        hucBoundaries.features[0].geometry.contains(location))
-    ) {
-      return getPopupContent({ feature: graphic.graphic });
-    }
-    return getPopupContent({
-      feature: graphic.graphic,
-      getClickedHuc: getClickedHuc(location),
-      resetData,
-    });
-  }
-
-  // Wrapper function for getting the title of the popup
-  function getTitle(graphic) {
-    return getPopupTitle(graphic.graphic.attributes);
-  }
-
   const retrieveUpstreamWatershed = React.useCallback(
     (
       currentHuc12,
@@ -913,6 +811,7 @@ function MapWidgets({
       Viewpoint,
       Graphic,
       services.data.upstreamWatershed,
+      getTitle,
     ],
   );
 
