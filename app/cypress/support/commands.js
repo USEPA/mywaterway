3508a8d5f3824454a4883006dbb47468cb726814
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2021-02-19 14:33:06 -0500

    Added tests for the add data widget.

--- app/cypress/support/commands.js
+++ app/cypress/support/commands.js
@@ -49,3 +49,40 @@ Cypress.Commands.add(
     });
   },
 );
+
+/**
+ * This enables uploading files with Cypress and the react-dropzone node module.
+ *
+ * @param subject - The react-dropzone element to upload the file with
+ * @param file - The file object to upload
+ * @param fileName - The name of the file being uploaded
+ */
+Cypress.Commands.add(
+  'upload',
+  {
+    prevSubject: 'element',
+  },
+  (subject, file, fileName, type) => {
+    // we need access window to create a file below
+    cy.window().then((window) => {
+      // Convert the file to a blob (if necessary) and upload
+      let contents = file;
+      if (type === 'blob') {
+        contents = Cypress.Blob.base64StringToBlob(file);
+      }
+      if (type === 'json') {
+        contents = JSON.stringify(file);
+      }
+
+      // Please note that we need to create a file using window.File,
+      // cypress overwrites File and this is not compatible with our change handlers in React Code
+      const testFile = new window.File([contents], fileName);
+
+      // trigger the drop event on the react-dropzone component
+      cy.wrap(subject).trigger('drop', {
+        force: true,
+        dataTransfer: { files: [testFile], types: ['Files'] },
+      });
+    });
+  },
+);
