e725e436b8f6f796ef3a2f6901fc85e9cf72217a
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2021-12-16 10:23:02 -0500

    Fixed regex code smell.

--- app/client/src/components/shared/Regex/index.js
+++ app/client/src/components/shared/Regex/index.js
@@ -1,7 +1,7 @@
 // returns the url from a stringified anchor tag's markup
 export function getUrlFromMarkup(markup: string) {
   // match on the url (text between the double quotes)
-  const match = new RegExp('.*?(".*?")', 'i').exec(markup);
+  const match = markup.match(/.*?(".*?")/i);
   if (!match) return '';
   // escape '<' characters, and strip leading and trailing double quotes
   return match[1].replace(/</, '&lt;').slice(1, -1);
@@ -9,7 +9,7 @@ export function getUrlFromMarkup(markup: string) {
 
 export function getTitleFromMarkup(markup: string) {
   // match on text between brackets i.e. <a ...>Match</a>
-  const match = new RegExp('>.*?<', 'i').exec(markup);
+  const match = markup.match(/>.*?</i);
   if (!match) return false;
   // remove > and < surrounding the text
   return match[0].slice(1, -1);
