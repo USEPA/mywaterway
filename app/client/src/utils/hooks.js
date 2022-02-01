aa678ff58d2cb0edee13702ae6ac8435d26d98ad
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2022-02-01 11:05:58 -0500

    HMW-101 Disabled highlighting of the surrounding waterbodies layer when the user clicks within the huc.

--- app/client/src/utils/hooks.js
+++ app/client/src/utils/hooks.js
@@ -226,13 +226,8 @@ function useWaterbodyOnMap(
     setHighlightedGraphic,
     setSelectedGraphic, //
   } = useContext(MapHighlightContext);
-  const {
-    allWaterbodiesLayer,
-    pointsLayer,
-    linesLayer,
-    areasLayer,
-    mapView,
-  } = useContext(LocationSearchContext);
+  const { allWaterbodiesLayer, pointsLayer, linesLayer, areasLayer, mapView } =
+    useContext(LocationSearchContext);
 
   const setRenderer = useCallback(
     (layer, geometryType, attribute, alpha = null) => {
@@ -407,11 +402,8 @@ function useWaterbodyHighlight(findOthers: boolean = true) {
     else if (selectedGraphic) graphic = selectedGraphic;
 
     // save the state into separate variables for now
-    let {
-      currentHighlight,
-      currentSelection,
-      cachedHighlights,
-    } = highlightState;
+    let { currentHighlight, currentSelection, cachedHighlights } =
+      highlightState;
 
     // verify that we have a graphic before continuing
     if (!graphic || !graphic.attributes) {
@@ -479,6 +471,7 @@ function useWaterbodyHighlight(findOthers: boolean = true) {
     }
 
     if (!layer) return;
+    if (graphic.layer?.parent?.id === 'allWaterbodiesLayer') return;
 
     // remove the highlights
     handles.remove(group);
