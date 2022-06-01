8258017d691b34f0d679cae739e19a1ae1be3e35
Courtney Myers <courtney.myers@erg.com>
2022-06-01 16:25:44 -0400

    Remove gageHeight from usgsStreamgagesLayer graphics, as its no longer used

--- app/client/src/components/shared/LocationMap.js
+++ app/client/src/components/shared/LocationMap.js
@@ -713,7 +713,6 @@ function LocationMap({ layout = 'narrow', windowHeight, children }: Props) {
       legendEnabled: false,
       fields: [
         { name: 'OBJECTID', type: 'oid' },
-        { name: 'gageHeight', type: 'string' },
         { name: 'monitoringType', type: 'string' },
         { name: 'siteId', type: 'string' },
         { name: 'orgId', type: 'string' },
@@ -741,25 +740,6 @@ function LocationMap({ layout = 'narrow', windowHeight, children }: Props) {
           style: 'square',
           color: '#fffe00', // '#989fa2'
         },
-        // NOTE: rendering all streamgages in a single color until we can set
-        //       color stops from data returned in USGS STA web service
-        // visualVariables: [
-        //   {
-        //     type: 'color',
-        //     field: 'gageHeight',
-        //     stops: [
-        //       // TODO: determine how to map of gage height values to NWD streamflow percentile stops
-        //       // (National Water Dashboard: https://dashboard.waterdata.usgs.gov/app/nwd/?aoi=default)
-        //       { value: '0', color: '#ea2c38' }, // All-time low for this day  (0th percentile, minimum)
-        //       { value: '1', color: '#b54246' }, // Much below normal          (<10th percentile)
-        //       { value: '2', color: '#eaae3f' }, // Below normal               (10th – 24th percentile)
-        //       { value: '3', color: '#32f242' }, // Normal                     (25th – 75th percentile)
-        //       { value: '4', color: '#56d7da' }, // Above normal               (76th – 90th percentile)
-        //       { value: '5', color: '#2639f6' }, // Much above normal          (>90th percentile)
-        //       { value: '6', color: '#22296e' }, // All-time high for this day (100th percentile, maximum)
-        //     ],
-        //   },
-        // ],
       },
       popupTemplate: {
         outFields: ['*'],
@@ -1311,22 +1291,13 @@ function LocationMap({ layout = 'narrow', windowHeight, children }: Props) {
     if (!usgsStreamgagesLayer || !normalizedUsgsStreamgages.length) return;
 
     const graphics = normalizedUsgsStreamgages.map((gage) => {
-      const gageHeightMeasurements = gage.streamgageMeasurements.primary.filter(
-        (d) => d.parameterCode === '00065',
-      );
-
-      const gageHeight =
-        gageHeightMeasurements.length === 1
-          ? gageHeightMeasurements[0]?.measurement
-          : null;
-
       return new Graphic({
         geometry: {
           type: 'point',
           longitude: gage.locationLongitude,
           latitude: gage.locationLatitude,
         },
-        attributes: { gageHeight, ...gage },
+        attributes: { gage },
       });
     });
 
