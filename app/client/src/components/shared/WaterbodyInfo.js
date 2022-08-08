fa4b4d42d158dcb78550b957c507c872f8c1e0f7
Max Diebold <max.diebold@erg.com>
2022-08-08 16:22:15 -0400

    Updated WaterbodyInfo to match new path

--- app/client/src/components/shared/WaterbodyInfo.js
+++ app/client/src/components/shared/WaterbodyInfo.js
@@ -1205,7 +1205,7 @@ function MonitoringLocationsContent({ attributes, services }) {
     `&providers=NWIS&providers=STEWARDS&providers=STORET`;
 
   const onMonitoringReportPage =
-    window.location.pathname.indexOf('station') === 1;
+    window.location.pathname.indexOf('location') === 1;
 
   return (
     <>
@@ -1251,7 +1251,7 @@ function MonitoringLocationsContent({ attributes, services }) {
             </td>
             <td>
               {Number(stationTotalSamples).toLocaleString()}
-              <span css={dateRangeStyles}>(all time)</span>
+              {timeframe && <span css={dateRangeStyles}>(all time)</span>}
             </td>
           </tr>
           <tr>
