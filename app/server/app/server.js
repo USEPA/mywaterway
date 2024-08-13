ddf4dd6ad164445fe7f07ace18140252b0f329da
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2024-08-13 08:06:03 -0400

    HMW-775 Renamed the 400.html file to 404.html.

--- app/server/app/server.js
+++ app/server/app/server.js
@@ -211,7 +211,7 @@ app.listen(port, function () {
 /* Note, the React app should be handling 404 at this point 
    but we're leaving the below 404 check in for now */
 app.use(function (req, res, next) {
-  res.sendFile(path.join(__dirname, 'public', '400.html'));
+  res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 app.use(function (err, req, res, next) {
