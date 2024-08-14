49f464599fdff756f6ce0c77e3b7f48280fe0325
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2024-08-14 12:13:26 -0400

    HMW-776 Updated code-coverage to work with vite.

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import istanbul from "vite-plugin-istanbul";
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
@@ -37,6 +38,10 @@ export default ({ mode }) => {
         babel: {
           plugins: ['@emotion/babel-plugin'],
         },
+      }),  
+      istanbul({
+        cypress: true,
+        requireEnv: false,
       }),
       viteTsconfigPaths(),
     ],
