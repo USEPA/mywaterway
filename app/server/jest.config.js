60150415a8838c109ced374e0505ad1efbb5f188
Brad Cooper <coobr01@yahoo.com>
2024-09-13 14:06:19 -0400

    Adjusting testing setup to support server-side tests and coverage

---
+++ app/server/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  setupFiles: ['./jest_setup.js'],
+  roots: [
+    "tests"
+  ],
+  testPathIgnorePatterns: [
+    "node_modules/",
+    "dist/",
+    "build/",
+    ".nyc_output/",
+    "coverage/"
+  ],
+};
