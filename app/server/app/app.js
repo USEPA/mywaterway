2c1fcc3ab3614243f187c086652b12259d594515
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2025-10-20 16:51:37 -0400

    HMW-942 Added task for caching usgs site types lookups.

--- app/server/app/app.js
+++ app/server/app/app.js
@@ -9,6 +9,7 @@ const { getEnvironment } = require('./server/utilities/environment');
 const logger = require('./server/utilities/logger');
 const { getS3Config } = require('./server/utilities/s3');
 const updateGlossary = require('./tasks/updateGlossary');
+const updateUsgsSiteTypes = require('./tasks/updateUsgsSiteTypes');
 const log = logger.logger;
 
 const app = express();
@@ -164,12 +165,14 @@ if (!isLocal && !isTest) {
 if (isLocal || process.env.CF_INSTANCE_INDEX === '0') {
   // run glossary task once at start-up
   updateGlossary();
+  updateUsgsSiteTypes();
 
   log.info('Scheduling glossary cron task to run every day at 1AM');
   cron.schedule(
     '0 1 * * *',
     () => {
       updateGlossary();
+      updateUsgsSiteTypes();
     },
     { scheduled: true },
   );
