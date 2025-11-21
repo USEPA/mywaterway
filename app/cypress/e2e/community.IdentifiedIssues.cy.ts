2487fbc386ca27f8b024759863611348270301c7
Caleb Schwind <46329268+cschwinderg@users.noreply.github.com>
2025-11-21 13:59:52 -0500

    Updated changelog and version numbers.

--- app/cypress/e2e/community.IdentifiedIssues.cy.ts
+++ app/cypress/e2e/community.IdentifiedIssues.cy.ts
@@ -46,7 +46,7 @@ describe('Identified Issues Tab', () => {
     cy.findByRole('tab', { name: /Permitted Dischargers/ }).click({
       force: true,
     });
-    cy.findByRole('switch', { name: /92 Permitted Dischargers/ }).should(
+    cy.findByRole('switch', { name: /97 Permitted Dischargers/ }).should(
       'have.attr',
       'aria-checked',
       'true',
